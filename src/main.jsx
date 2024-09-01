9aa148d956f850f784ae58a1d5640390100863fc
Heorhii Honcharov <0019@ukr.net>
2024-09-01 11:51:18 +0200

    init

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,10 @@ import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
-import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/react_vite">
+    <BrowserRouter basename="/test-smart">
       <App />
     </BrowserRouter>
   </React.StrictMode>,
